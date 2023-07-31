4a90f3e0e945c58a7cc26753083e70a1fddf5dad
Nsiva <webertamil@gmail.com>
2023-07-31 06:50:36 -0400

    First Commit

---
+++ sub.java
@@ -0,0 +1,11 @@
+public class Demo{
+
+
+    public static void main(){
+            int a=10;
+            int b=10;
+            int c=10;
+
+            System.out.println("add"+a+b+c);
+    }
+}
